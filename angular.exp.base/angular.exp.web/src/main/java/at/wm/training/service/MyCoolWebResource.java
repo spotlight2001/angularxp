66ce4f42873ef70da955fe8d6fd6b6a8bb516ff4
walter <waltermauritz@gmx.at>
2013-10-05 21:55:23 +0200

    .

--- angular.exp.base/angular.exp.web/src/main/java/at/wm/training/service/MyCoolWebResource.java
+++ angular.exp.base/angular.exp.web/src/main/java/at/wm/training/service/MyCoolWebResource.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,9 @@ public class MyCoolWebResource {
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(MyCoolWebResource.class);
+
+	@Value("#{dbcfg.app.url}")
+	private String appUrl;
 
 	@RequestMapping("{id}")
 	@ResponseBody
